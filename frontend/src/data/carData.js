b1dcfd60c6cdf0283c36d662385af823f204bb2a
Mohanad Alsalom <mohanad.altaai@gmail.com>
2026-05-06 04:34:12 +0300

    Expand car makes and models

--- frontend/src/data/carData.js
+++ frontend/src/data/carData.js
@@ -1,10 +1,312 @@
-export const carData = {
-  Japanese: { emoji: '🇯🇵', makes: { Toyota: ['Camry','Corolla','RAV4','Land Cruiser','Fortuner'], Honda: ['Civic','Accord','CR-V','HR-V'], Nissan: ['Altima','Sentra','Patrol','X-Trail'], Mazda: ['CX-5','Mazda3','Mazda6'] } },
-  German: { emoji: '🇩🇪', makes: { BMW: ['3 Series','5 Series','X3','X5'], Mercedes: ['C-Class','E-Class','S-Class','GLE'], Audi: ['A3','A4','Q5','Q7'], VW: ['Golf','Tiguan','Passat'] } },
-  American: { emoji: '🇺🇸', makes: { Ford: ['F-150','Mustang','Explorer','Ranger'], Chevy: ['Silverado','Tahoe','Malibu'], Dodge: ['Charger','Challenger','Durango'], Jeep: ['Wrangler','Grand Cherokee','Compass'] } },
-  Korean: { emoji: '🇰🇷', makes: { Hyundai: ['Elantra','Sonata','Tucson','Santa Fe'], Kia: ['Sportage','Sorento','K5','Cerato'], Genesis: ['G70','G80','GV70'] } },
-  Chinese: { emoji: '🇨🇳', makes: { Chery: ['Tiggo 7','Tiggo 8','Arrizo 5'], MG: ['MG5','HS','ZS'], Haval: ['H6','Jolion','H9'], Geely: ['Coolray','Tugella','Emgrand'], BYD: ['Song','Han','Atto 3'], Changan: ['CS35','CS75','Uni-T'], Jetour: ['X70','Dashing','T2'] } },
-  French: { emoji: '🇫🇷', makes: { Peugeot: ['208','308','3008','5008'], Renault: ['Duster','Megane','Koleos'], Citroen: ['C3','C4','C5 Aircross'] } }
+﻿export const carData = {
+  Japanese: {
+    emoji: '🇯🇵',
+    makes: {
+      Toyota: [
+        'Camry', 'Corolla', 'Yaris', 'Avalon', 'Crown', 'Prius',
+        'RAV4', 'Highlander', 'Land Cruiser', 'Land Cruiser Prado',
+        'Fortuner', 'Hilux', 'Tacoma', 'Tundra', 'Sequoia',
+        'Innova', 'Urban Cruiser', 'C-HR', 'Raize', 'Rush'
+      ],
+      Lexus: [
+        'IS', 'ES', 'GS', 'LS', 'UX', 'NX', 'RX', 'GX', 'LX',
+        'RC', 'LC'
+      ],
+      Honda: [
+        'Civic', 'Accord', 'City', 'CR-V', 'HR-V', 'Pilot',
+        'Odyssey', 'Fit', 'Jazz', 'Ridgeline'
+      ],
+      Nissan: [
+        'Sunny', 'Sentra', 'Altima', 'Maxima', 'Patrol', 'X-Trail',
+        'Pathfinder', 'Kicks', 'Juke', 'Murano', 'Navara', 'Armada',
+        'Tiida', 'Urvan'
+      ],
+      Mazda: [
+        'Mazda2', 'Mazda3', 'Mazda6', 'CX-3', 'CX-30', 'CX-5',
+        'CX-7', 'CX-9', 'BT-50'
+      ],
+      Mitsubishi: [
+        'Lancer', 'Attrage', 'Mirage', 'ASX', 'Outlander',
+        'Pajero', 'Pajero Sport', 'Montero Sport', 'L200'
+      ],
+      Subaru: [
+        'Impreza', 'Legacy', 'Forester', 'Outback', 'XV', 'WRX',
+        'BRZ', 'Ascent'
+      ],
+      Suzuki: [
+        'Swift', 'Dzire', 'Ciaz', 'Baleno', 'Vitara', 'Grand Vitara',
+        'Jimny', 'Ertiga', 'S-Cross', 'Carry'
+      ],
+      Isuzu: [
+        'D-Max', 'MU-X', 'N-Series', 'F-Series'
+      ],
+      Infiniti: [
+        'Q30', 'Q50', 'Q60', 'QX30', 'QX50', 'QX55', 'QX60',
+        'QX70', 'QX80'
+      ]
+    }
+  },
+
+  Korean: {
+    emoji: '🇰🇷',
+    makes: {
+      Hyundai: [
+        'Accent', 'Elantra', 'Avante', 'Sonata', 'Azera', 'Grandeur',
+        'Tucson', 'Santa Fe', 'Palisade', 'Creta', 'Venue',
+        'Kona', 'Staria', 'H1', 'H100', 'i10', 'i20', 'i30',
+        'Genesis Coupe'
+      ],
+      Kia: [
+        'Picanto', 'Rio', 'Cerato', 'Forte', 'K3', 'K5', 'Optima',
+        'Cadenza', 'K7', 'K8', 'Stinger', 'Soul', 'Seltos',
+        'Sportage', 'Sorento', 'Telluride', 'Carnival', 'Carens',
+        'Mohave', 'Bongo'
+      ],
+      Genesis: [
+        'G70', 'G80', 'G90', 'GV60', 'GV70', 'GV80'
+      ],
+      SsangYong: [
+        'Tivoli', 'Korando', 'Rexton', 'Musso', 'Actyon'
+      ],
+      Daewoo: [
+        'Lanos', 'Nubira', 'Leganza', 'Matiz', 'Lacetti'
+      ]
+    }
+  },
+
+  German: {
+    emoji: '🇩🇪',
+    makes: {
+      BMW: [
+        '1 Series', '2 Series', '3 Series', '4 Series', '5 Series',
+        '6 Series', '7 Series', '8 Series', 'X1', 'X2', 'X3',
+        'X4', 'X5', 'X6', 'X7', 'Z4', 'M3', 'M4', 'M5'
+      ],
+      Mercedes: [
+        'A-Class', 'B-Class', 'C-Class', 'E-Class', 'S-Class',
+        'CLA', 'CLS', 'GLA', 'GLB', 'GLC', 'GLE', 'GLS',
+        'G-Class', 'V-Class', 'Sprinter', 'Vito', 'AMG GT'
+      ],
+      Audi: [
+        'A1', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8',
+        'Q2', 'Q3', 'Q5', 'Q7', 'Q8', 'TT', 'R8'
+      ],
+      Volkswagen: [
+        'Golf', 'Polo', 'Jetta', 'Passat', 'Arteon', 'Tiguan',
+        'Touareg', 'Teramont', 'T-Roc', 'Caddy', 'Transporter'
+      ],
+      Porsche: [
+        '911', '718 Boxster', '718 Cayman', 'Panamera', 'Macan',
+        'Cayenne', 'Taycan'
+      ],
+      Opel: [
+        'Astra', 'Corsa', 'Insignia', 'Mokka', 'Crossland',
+        'Grandland', 'Vectra'
+      ],
+      Mini: [
+        'Cooper', 'Clubman', 'Countryman', 'Paceman'
+      ],
+      Smart: [
+        'Fortwo', 'Forfour'
+      ]
+    }
+  },
+
+  American: {
+    emoji: '🇺🇸',
+    makes: {
+      Ford: [
+        'F-150', 'F-250', 'F-350', 'Ranger', 'Maverick', 'Mustang',
+        'Explorer', 'Expedition', 'Escape', 'Edge', 'Bronco',
+        'Bronco Sport', 'Focus', 'Fusion', 'Taurus', 'Transit'
+      ],
+      Chevrolet: [
+        'Spark', 'Aveo', 'Sonic', 'Cruze', 'Malibu', 'Impala',
+        'Camaro', 'Corvette', 'Trax', 'Equinox', 'Blazer',
+        'Traverse', 'Tahoe', 'Suburban', 'Silverado', 'Colorado',
+        'Express'
+      ],
+      GMC: [
+        'Terrain', 'Acadia', 'Yukon', 'Yukon XL', 'Sierra',
+        'Canyon', 'Savana'
+      ],
+      Cadillac: [
+        'ATS', 'CTS', 'CT4', 'CT5', 'CT6', 'XT4', 'XT5', 'XT6',
+        'Escalade', 'SRX'
+      ],
+      Dodge: [
+        'Charger', 'Challenger', 'Durango', 'Journey', 'Caravan',
+        'Ram', 'Dart'
+      ],
+      Jeep: [
+        'Wrangler', 'Grand Cherokee', 'Cherokee', 'Compass',
+        'Renegade', 'Gladiator', 'Commander', 'Patriot'
+      ],
+      Chrysler: [
+        '300', '200', 'Pacifica', 'Town & Country', 'Voyager'
+      ],
+      Lincoln: [
+        'MKZ', 'MKC', 'MKS', 'MKX', 'Navigator', 'Aviator',
+        'Corsair', 'Nautilus'
+      ],
+      Tesla: [
+        'Model 3', 'Model S', 'Model X', 'Model Y', 'Cybertruck'
+      ],
+      Hummer: [
+        'H2', 'H3', 'EV'
+      ]
+    }
+  },
+
+  Chinese: {
+    emoji: '🇨🇳',
+    makes: {
+      MG: [
+        'MG3', 'MG4', 'MG5', 'MG6', 'ZS', 'HS', 'RX5', 'RX8',
+        'Marvel R', 'One'
+      ],
+      Chery: [
+        'Arrizo 5', 'Arrizo 6', 'Tiggo 2', 'Tiggo 3', 'Tiggo 4',
+        'Tiggo 5', 'Tiggo 7', 'Tiggo 8', 'Tiggo 9'
+      ],
+      Exeed: [
+        'LX', 'TXL', 'VX', 'RX'
+      ],
+      Haval: [
+        'H2', 'H6', 'H9', 'Jolion', 'Dargo', 'Big Dog',
+        'Cool Dog'
+      ],
+      GreatWall: [
+        'Wingle', 'Poer', 'Cannon'
+      ],
+      Geely: [
+        'Emgrand', 'Coolray', 'Tugella', 'Okavango', 'Azkarra',
+        'Monjaro', 'Geometry C', 'Preface'
+      ],
+      BYD: [
+        'F3', 'Qin', 'Song', 'Tang', 'Han', 'Dolphin', 'Seal',
+        'Atto 3', 'Yuan Plus'
+      ],
+      Changan: [
+        'Alsvin', 'Eado', 'CS35', 'CS55', 'CS75', 'CS85',
+        'CS95', 'UNI-T', 'UNI-K', 'UNI-V'
+      ],
+      Jetour: [
+        'X70', 'X70 Plus', 'X90', 'X95', 'Dashing', 'T1', 'T2'
+      ],
+      GAC: [
+        'GA4', 'GA6', 'GA8', 'GS3', 'GS4', 'GS5', 'GS8',
+        'Empow', 'Emkoo'
+      ],
+      BAIC: [
+        'BJ40', 'BJ60', 'X35', 'X55', 'X7'
+      ],
+      JAC: [
+        'J7', 'S3', 'S4', 'S5', 'S7', 'T6', 'T8'
+      ],
+      Hongqi: [
+        'H5', 'H7', 'H9', 'HS3', 'HS5', 'HS7', 'E-HS9'
+      ],
+      Omoda: [
+        'C5', 'E5'
+      ],
+      Jaecoo: [
+        'J7', 'J8'
+      ],
+      Tank: [
+        '300', '500'
+      ]
+    }
+  },
+
+  French: {
+    emoji: '🇫🇷',
+    makes: {
+      Peugeot: [
+        '206', '207', '208', '301', '307', '308', '408',
+        '508', '2008', '3008', '5008', 'Partner', 'Expert'
+      ],
+      Renault: [
+        'Logan', 'Symbol', 'Megane', 'Fluence', 'Duster',
+        'Koleos', 'Captur', 'Talisman', 'Clio', 'Trafic'
+      ],
+      Citroen: [
+        'C1', 'C3', 'C4', 'C5', 'C-Elysee', 'C3 Aircross',
+        'C4 Aircross', 'C5 Aircross', 'Berlingo'
+      ],
+      DS: [
+        'DS3', 'DS4', 'DS5', 'DS7', 'DS9'
+      ]
+    }
+  },
+
+  British: {
+    emoji: '🇬🇧',
+    makes: {
+      LandRover: [
+        'Defender', 'Discovery', 'Discovery Sport', 'Range Rover',
+        'Range Rover Sport', 'Range Rover Velar', 'Range Rover Evoque'
+      ],
+      Jaguar: [
+        'XE', 'XF', 'XJ', 'F-Type', 'E-Pace', 'F-Pace', 'I-Pace'
+      ],
+      Mini: [
+        'Cooper', 'Clubman', 'Countryman', 'Paceman'
+      ],
+      Bentley: [
+        'Continental GT', 'Flying Spur', 'Bentayga', 'Mulsanne'
+      ],
+      RollsRoyce: [
+        'Ghost', 'Phantom', 'Wraith', 'Dawn', 'Cullinan'
+      ],
+      AstonMartin: [
+        'DB9', 'DB11', 'DB12', 'Vantage', 'Vanquish', 'DBX'
+      ],
+      McLaren: [
+        '570S', '600LT', '720S', 'Artura', 'GT'
+      ]
+    }
+  },
+
+  Italian: {
+    emoji: '🇮🇹',
+    makes: {
+      Fiat: [
+        '500', 'Panda', 'Tipo', 'Punto', 'Doblo', 'Ducato',
+        'Fullback'
+      ],
+      AlfaRomeo: [
+        'Giulia', 'Giulietta', 'Stelvio', 'Tonale', 'MiTo'
+      ],
+      Maserati: [
+        'Ghibli', 'Quattroporte', 'Levante', 'Grecale',
+        'GranTurismo'
+      ],
+      Ferrari: [
+        'California', 'Portofino', 'Roma', '458', '488', 'F8',
+        'SF90', 'Purosangue'
+      ],
+      Lamborghini: [
+        'Gallardo', 'Huracan', 'Aventador', 'Urus'
+      ]
+    }
+  },
+
+  Swedish: {
+    emoji: '🇸🇪',
+    makes: {
+      Volvo: [
+        'S40', 'S60', 'S80', 'S90', 'V40', 'V60', 'V90',
+        'XC40', 'XC60', 'XC70', 'XC90'
+      ],
+      Saab: [
+        '9-3', '9-5'
+      ],
+      Polestar: [
+        'Polestar 1', 'Polestar 2', 'Polestar 3', 'Polestar 4'
+      ]
+    }
+  }
 };
 
-export const years = Array.from({ length: 30 }, (_, i) => new Date().getFullYear() - i);
+export const years = Array.from({ length: 35 }, (_, i) => new Date().getFullYear() - i);
